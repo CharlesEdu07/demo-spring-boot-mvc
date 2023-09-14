631a9817ea2d2235bd5673caad96ea5298ee0f12
unknown <charleseduardofaria@gmail.com>
2023-09-14 16:20:48 -0300

    Criando projeto

---
+++ src/main/java/com/charlesedu/course/DemoMvcApplication.java
@@ -0,0 +1,13 @@
+package com.charlesedu.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMvcApplication.class, args);
+	}
+
+}
